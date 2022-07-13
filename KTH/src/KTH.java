92aef1f4fb6fc23e0f00f5a68d9b44b8e2732f8a
EZEN-127 <EZEN-127@DESKTOP-U012RFH>
2022-07-13 15:45:30 +0900

    학원동기정보

---
+++ KTH/src/KTH.java
@@ -0,0 +1,15 @@
+
+public class KTH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("OGH, KMJ, KSJ");
+		System.out.println("KJS, NKH, LDH");
+		System.out.println("PYM, SDY, ABK");
+		System.out.println("CEJ, LYB, LJH");
+		System.out.println("JYS, JHS, HWJ");
+		
+	}
+
+}
